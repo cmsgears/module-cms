071bb656ae4116c08afc9bf19bc7caacd2db80e1
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-09-21 12:51:41 +0530

    Removed contact and feedback from system pages.

--- migrations/db-testdata.sql
+++ migrations/db-testdata.sql
@@ -37,12 +37,10 @@ INSERT INTO `cmg_cms_page` VALUES
 	(5,NULL,1,1,'Activate Account','activate-account','page',5,5),
 	(6,NULL,1,1,'Forgot Password','forgot-password','page',5,5),
 	(7,NULL,1,1,'Reset Password','reset-password','page',5,5),
-	(8,NULL,1,1,'Contact Us','contact','page',5,5),
-	(9,NULL,1,1,'Feedback','feedback','page',5,5),
-	(10,NULL,1,1,'About Us','about','page',5,5),
-	(11,NULL,1,1,'Terms','terms','page',5,5),
-	(12,NULL,1,1,'Privacy','privacy','page',5,5),
-	(13,NULL,1,1,'Blog','blog','page',5,5);
+	(8,NULL,1,1,'About Us','about','page',5,5),
+	(9,NULL,1,1,'Terms','terms','page',5,5),
+	(10,NULL,1,1,'Privacy','privacy','page',5,5),
+	(11,NULL,1,1,'Blog','blog','page',5,5);
 
 --
 -- Dumping data for table `cmg_cms_model_content`
@@ -56,11 +54,9 @@ INSERT INTO `cmg_cms_model_content` VALUES
 	(5,NULL,NULL,NULL,5,'page',NULL,NULL,'2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'activate account page',NULL,NULL),
 	(6,NULL,NULL,NULL,6,'page',NULL,NULL,'2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'forgot password page',NULL,NULL),
 	(7,NULL,NULL,NULL,7,'page',NULL,NULL,'2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'reset password page',NULL,NULL),
-	(8,NULL,NULL,NULL,8,'page',NULL,NULL,'2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'contact page',NULL,NULL),
-	(9,NULL,NULL,NULL,9,'page',NULL,NULL,'2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'feedback page',NULL,NULL),
-	(10,NULL,NULL,NULL,10,'page','<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'about us page',NULL,NULL),
-	(11,NULL,NULL,NULL,11,'page',NULL,'<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'terms page',NULL,NULL),
-	(12,NULL,NULL,NULL,12,'page',NULL,'<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'privacy page',NULL,NULL),
-	(13,NULL,NULL,NULL,13,'page',NULL,NULL,'2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'blog page',NULL,NULL);
+	(8,NULL,NULL,NULL,8,'page','<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'about us page',NULL,NULL),
+	(9,NULL,NULL,NULL,9,'page',NULL,'<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'terms page',NULL,NULL),
+	(10,NULL,NULL,NULL,10,'page',NULL,'<p>Lorem ipsum is a pseudo-Latin text used in web design, typography, layout, and printing in place of English to emphasise design elements over content. It\'s also called placeholder (or filler) text. It\'s a convenient tool for mock-ups. It helps to outline the visual elements of a document or presentation, eg typography, font, or layout. Lorem ipsum is mostly a part of a Latin text by the classical author and philosopher Cicero</p>','2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'privacy page',NULL,NULL),
+	(11,NULL,NULL,NULL,11,'page',NULL,NULL,'2014-10-01 00:00:00','2014-10-01 00:00:00','2014-10-01 00:00:00',NULL,'blog page',NULL,NULL);
 
 SET FOREIGN_KEY_CHECKS=1;
