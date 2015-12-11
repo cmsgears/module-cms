6cecfdd061e06cbaf74cd5088900bab7fc7cf520
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-12-11 18:57:37 +0530

    Added page attribute to db drop.

--- migrations/innodb/db-drop.sql
+++ migrations/innodb/db-drop.sql
@@ -4,6 +4,8 @@ SET FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE IF EXISTS `cmg_cms_page`;
 
+DROP TABLE IF EXISTS `cmg_cms_page_attribute`;
+
 DROP TABLE IF EXISTS `cmg_cms_block`;
 
 DROP TABLE IF EXISTS `cmg_cms_model_content`;
