cc54658369ea05616943eeef12f5c48c4e258bd3
dam <dharmendra.prajapati@vulpinecode.com>
2017-09-08 10:22:10 +0530

    Resolved comments url issue.

--- admin/views/templates/grid/actions/post.php
+++ admin/views/templates/grid/actions/post.php
@@ -1,7 +1,7 @@
 <?php
 use yii\helpers\Html;
 ?>
-<span title="Comments"><?= Html::a( "", [ "comment/all?pid=$model->id" ], [ 'class' => 'cmti cmti-comment' ] ) ?></span>
+<span title="Comments"><?= Html::a( "", [ "post/comment/all?pid=$model->id" ], [ 'class' => 'cmti cmti-comment' ] ) ?></span>
 <span title="Gallery"><?= Html::a( "", [ "post/gallery/index?pid=$model->id" ], [ 'class' => 'cmti cmti-image' ] ) ?></span>
 <span title="Update"><?= Html::a( "", [ "update?id=$model->id" ], [ 'class' => 'cmti cmti-edit' ] )  ?></span>
 
